6e80555fc1d6513b0140544a22a03dc7c702c4a7
王宇轩 <165140760@qq.com>
2020-04-29 14:16:06 +0800

    Mybatis

---
+++ Mybatis/src/main/java/com/wangyuxuan/mybatis/demo1/JdbcDemo.java
@@ -0,0 +1,67 @@
+package com.wangyuxuan.mybatis.demo1;
+
+import org.junit.Test;
+
+import java.sql.*;
+
+/**
+ * @author wangyuxuan
+ * @date 2020/4/29 13:58
+ * @description JdbcDemo
+ */
+public class JdbcDemo {
+
+    @Test
+    public void test() {
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            // 加载数据库驱动
+            Class.forName("com.mysql.jdbc.Driver");
+            // 获取数据库链接
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mybatis?characterEncoding=utf-8", "root", "123456");
+            // 定义SQL语句
+            String sql = "select * from user where username = ?";
+            // 获取statement
+            preparedStatement = connection.prepareStatement(sql);
+            // 设置参数
+            preparedStatement.setString(1, "wangyuxuan");
+            // 执行SQL 获取结果集
+            resultSet = preparedStatement.executeQuery();
+            // 遍历输出查询结果集
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("id") + " " + resultSet.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // 释放资源
+            if (resultSet != null) {
+                try {
+                    resultSet.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 释放资源
+            if (preparedStatement != null) {
+                try {
+                    preparedStatement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            // 释放资源
+            if (connection != null) {
+                try {
+                    connection.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
