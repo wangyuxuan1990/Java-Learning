08522fcb13b8bcba6e7a22dab1f1572e3c3b8679
王宇轩 <165140760@qq.com>
2020-05-02 13:17:18 +0800

    Update MybatisV1.java

--- Mybatis/src/main/java/com/wangyuxuan/mybatis/demo1/MybatisV1.java
+++ Mybatis/src/main/java/com/wangyuxuan/mybatis/demo1/MybatisV1.java
@@ -8,9 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Field;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * @author wangyuxuan
@@ -21,33 +19,45 @@ public class MybatisV1 {
 
     private Properties properties = new Properties();
 
-    private static String DRIVER = "db.driver";
+    private static final String DRIVER = "db.driver";
 
-    /**
-     * 加载properties配置文件
-     */
-    public void loadProperties() {
+    @Test
+    public void test() {
+        loadProperties();
+        Map<String, Object> param1 = new HashMap<>();
+        param1.put("username", "wangyuxuan");
+        param1.put("sex", 1);
+        User param2 = new User();
+        param2.setUsername("wangyuxuan");
+        param2.setSex("1");
+        List<User> result1 = selectList("queryUserById", param1);
+        List<User> result2 = selectList("queryUserById", param2);
+        System.out.println(result1);
+        System.out.println(result2);
+    }
+
+    private void loadProperties() {
+        InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("jdbc.properties");
         try {
-            InputStream inputStream = this.getClass().getClassLoader().getResourceAsStream("jdbc.properties");
             properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (inputStream != null) {
+                try {
+                    inputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 
-    /**
-     * 执行JDBC代码，完成查询用户操作
-     *
-     * @param statementId
-     * @param paramObject
-     * @return
-     */
-    public List<Object> selectList(String statementId, Object paramObject) {
+    private <T> List<T> selectList(String statementId, Object param) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
-        // 定义返回的集合
-        List<Object> results = new ArrayList<>();
+        List<T> resultList = new ArrayList<>();
         try {
             // 加载数据库驱动
             Class.forName(properties.getProperty(DRIVER));
@@ -57,44 +67,41 @@ public class MybatisV1 {
             String sql = properties.getProperty("db.sql." + statementId);
             // 获取statement
             preparedStatement = connection.prepareStatement(sql);
-            // 先获取参数类型
-            String parameterType = properties.getProperty("db.sql." + statementId + ".parametertype");
-            Class<?> parameterTypeClass = Class.forName(parameterType);
-            // 判断输入参数类型
-            if (SimpleTypeRegistry.isSimpleType(parameterTypeClass)) {
-                preparedStatement.setObject(1, paramObject);
+            Class<?> paramClassType = param.getClass();
+            // 设置参数
+            if (SimpleTypeRegistry.isSimpleType(paramClassType)) {
+                preparedStatement.setObject(1, param);
             } else {
-                // 遍历SQL语句当中的参数列表
                 String params = properties.getProperty("db.sql." + statementId + ".params");
-                String[] paramArray = params.split(",");
-                for (int i = 0; i < paramArray.length; i++) {
-                    // 根据列名获取入参对象的属性值，前提：列名和属性名称要一致
-                    Field field = parameterTypeClass.getDeclaredField(paramArray[i]);
-                    field.setAccessible(true);
-                    // 获取属性值
-                    Object value = field.get(paramObject);
+                String[] paramsArray = params.split(",");
+                for (int i = 0; i < paramsArray.length; i++) {
+                    Object value = null;
+                    if (param instanceof Map) {
+                        value = ((Map) param).get(paramsArray[i]);
+                    } else {
+                        Field field = paramClassType.getDeclaredField(paramsArray[i]);
+                        field.setAccessible(true);
+                        value = field.get(param);
+                    }
                     preparedStatement.setObject(i + 1, value);
                 }
             }
             // 执行SQL 获取结果集
             resultSet = preparedStatement.executeQuery();
-            // 获取要映射的结果类型
-            String resultclassname = properties.getProperty("db.sql." + statementId + ".resultclassname");
-            Class<?> resultTypeClass = Class.forName(resultclassname);
-            Object result = null;
+            String resultClassName = properties.getProperty("db.sql." + statementId + ".resultclassname");
+            Class<?> resultClassType = Class.forName(resultClassName);
+            // 遍历输出查询结果集
             while (resultSet.next()) {
-                // 每一行对应一个对象
-                result = resultTypeClass.newInstance();
-                ResultSetMetaData metaData = resultSet.getMetaData();
-                int columnCount = metaData.getColumnCount();
+                Object result = resultClassType.newInstance();
+                ResultSetMetaData resultSetMetaData = resultSet.getMetaData();
+                int columnCount = resultSetMetaData.getColumnCount();
                 for (int i = 1; i <= columnCount; i++) {
-                    // 获取结果集中的列名
-                    String columnName = metaData.getColumnName(i);
-                    Field field = resultTypeClass.getDeclaredField(columnName);
+                    String columnName = resultSetMetaData.getColumnName(i);
+                    Field field = resultClassType.getDeclaredField(columnName);
                     field.setAccessible(true);
                     field.set(result, resultSet.getObject(columnName));
                 }
-                results.add(result);
+                resultList.add((T) result);
             }
         } catch (Exception e) {
             e.printStackTrace();
@@ -124,15 +131,6 @@ public class MybatisV1 {
                 }
             }
         }
-        return results;
-    }
-
-    @Test
-    public void test() {
-        loadProperties();
-        User user = new User();
-        user.setUsername("wangyuxuan");
-        List<Object> list = selectList("queryUserById", user);
-        System.out.println(list);
+        return resultList;
     }
 }
